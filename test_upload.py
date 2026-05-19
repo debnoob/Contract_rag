bff08bdf31204b49745434a9863bfd4615db7079
debnoob <debarghya2953@gmail.com~>
2026-05-19 14:17:35 +0530

    first commit

---
+++ test_upload.py
@@ -0,0 +1,5 @@
+import requests
+files = {'file': ('test.pdf', b'fake pdf content', 'application/pdf')}
+data = {'insurer': 'General', 'category': 'Policy'}
+r = requests.post('http://127.0.0.1:8000/api/upload', files=files, data=data)
+print(r.status_code, r.text)
